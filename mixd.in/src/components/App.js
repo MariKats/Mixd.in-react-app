d58e3062997b291e6cb3fd04d75b6b0cca816e1e
github username <github email address>
2017-06-19 15:23:59 -0400

    very simple about page

--- mixd.in/src/components/App.js
+++ mixd.in/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Panel } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom'
 import NavBar from './NavBar';
 import DrinkPage from './DrinkPage';
@@ -17,8 +18,25 @@ class App extends Component {
           <Route exact path='/' render={() => <Landing />} />
           <Route path='/drinks' render={() => <DrinkPage />} />
           <Route path='/drinks/:id/make' render={() => <StepsContainer />} />
+          <Route path="/about" render={() => {
+            return (
+              <div className="container">
+                <div className="row row-centered">
+                    <div className="col-xs-6 col-centered">
+                      <Panel className="about text-center">
+                        <p>This is an app for drink lovers.</p>
+                        <p>Users can search for their favorite recipes, either by name or by ingredient.</p>
+                        <p>Users can also add to the collection by posting their own recipes.</p>
+                        <p>Lastly, the app guides users step-by-step through the drink-making process.</p>
+                      </Panel>
+                    </div>
+                </div>
+            </div>
+
+            )
+          }} />
         </Switch>
-        
+
       </div>
     );
   }
